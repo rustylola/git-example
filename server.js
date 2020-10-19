8b81f1adb1e7484e486f2747129cacf28c774acf
rustylola <johnrustylola@gmail.com>
2020-10-19 17:55:50 +0800

    server edit

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const PORT = process.env.PORT || 8080;
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
-  console.log(`Listening on port: ${PORT}`);
-
-
-}
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
